2a309edada6400c657f88675ad27156628d2351e
sunny8200118 <sunny8200118@gmail.com>
2019-07-01 17:01:28 +0800

    側邊欄廣告連結fix

--- aotter-assignment-sdk-WST/js/all.js
+++ aotter-assignment-sdk-WST/js/all.js
@@ -5,28 +5,35 @@ console.log(xhr);
 xhr.onload = function () {
     const xhrArr = JSON.parse(xhr.responseText);
     const lgAd = document.querySelector('.lg-ad a');
-    const lgAdContent = document.createElement('iframe');
+    const adList = document.getElementById('ad-list');
+    const lgAdContent = document.createElement('img');
+    const lgAdVideo = document.createElement('iframe');
     const AdTitle = document.createElement('h2');
     const AdText = document.createElement('p');
     // 隨機置入大廣告 //
     const num = Math.floor(Math.random() * xhrArr.length);
-    lgAd.setAttribute('style',`width:${window.innerWidth / 2}px;height:${window.innerWidth / 2 / 16 * 9}px;`);
+    lgAd.setAttribute('style', `width:${window.innerWidth / 2}px;height:${window.innerWidth / 2 / 16 * 9}px;`);
     // 判斷大廣告是否置入成功
-    if(xhrArr[num].success == true){  
+    if (xhrArr[num].success == true) {
         console.log(`on-ad-loaded:${xhrArr[num].id}`);
-    }else{
+    } else {
         console.log('on-ad-failed');
     }
-    // 判斷type種類
+    // 判斷type種類，並置入頁面
+    function checkType(href, src, append) {
+        lgAd.setAttribute('href', href);
+        lgAdContent.setAttribute('src', src);
+        lgAd.appendChild(append);
+    }
     if (xhrArr[num].type == 'BANNER') {
-        lgAd.setAttribute('href', xhrArr[num].url);
-        lgAdContent.setAttribute('src', xhrArr[num].image);
+        checkType(xhrArr[num].image, xhrArr[num].image, lgAdContent);
+        lgAd.appendChild(lgAdContent);
     } else if (xhrArr[num].type == 'VIDEO') {
-        lgAd.setAttribute('href', 'javascript:void(0);');
-        lgAdContent.setAttribute('src', xhrArr[num].video_url);
+        checkType('javascript:void(0);', xhrArr[num].video_url, lgAdVideo);
+        lgAd.appendChild(lgAdVideo);
     } else {
-        lgAd.setAttribute('href', 'javascript:void(0);');
-        lgAdContent.setAttribute('src', '../img/default.png');
+        checkType('javascript:void(0);', '../img/default.png', lgAdContent);
+        lgAd.appendChild(lgAdContent);
     }
     // 置入標題
     AdTitle.setAttribute('style', `width:${window.innerWidth / 2}px`);
@@ -35,14 +42,13 @@ xhr.onload = function () {
     AdText.setAttribute('style', `width:${window.innerWidth / 2}px`);
     AdText.textContent = xhrArr[num].description;
 
-    lgAd.appendChild(lgAdContent);
     lgAd.appendChild(AdTitle);
     lgAd.appendChild(AdText);
     const closeBtn = document.getElementById('close');
 
     function showCloceBtn() {
         closeBtn.style.opacity = '1';
-        if (xhrArr[num].success == true){
+        if (xhrArr[num].success == true) {
             console.log(xhrArr[num].impression_url);
         }
     }
@@ -58,13 +64,21 @@ xhr.onload = function () {
     let str = '';
     for (let i = 0; i < xhrArr.length; i++) {
         if (xhrArr[i].success == true) {
-            str += `<div class="ad">
-            <img src="${xhrArr[i].image}" alt="${xhrArr[i].title}" class="img-fluid">
-            <h2>${xhrArr[i].title}</h2>
-            <p>${xhrArr[i].description}</p>
-            </div>`;
+            if (xhrArr[i].type == 'BANNER') {
+                str += `<a href="${xhrArr[i].url}">
+                <img src="${xhrArr[i].image}" alt="${xhrArr[i].title}" class="img-fluid">
+                <h2>${xhrArr[i].title}</h2>
+                <p>${xhrArr[i].description}</p>
+                </a>`;
+            } else if (xhrArr[i].type == 'VIDEO') {
+                str += `<a href="${xhrArr[i].video_url}">
+                <img src="${xhrArr[i].image}" alt="${xhrArr[i].title}" class="img-fluid">
+                <h2>${xhrArr[i].title}</h2>
+                <p>${xhrArr[i].description}</p>
+                </a>`;
+            }
         }
     }
-    document.getElementById('ad-list').innerHTML = str;
+    adList.innerHTML = str;
     // END 置入側邊廣告 //
 }
